8f634036ef42e1941fcada2cec233b2334df17d0
ijaz-44 <123962078+ijaz-44@users.noreply.github.com>
2026-03-28 03:40:22 +0500

    🔄 Update my_automation_backup/data_sources/__init__.py via Automation

--- my_automation_backup/data_sources/__init__.py
+++ my_automation_backup/data_sources/__init__.py
@@ -1,1 +1,13 @@
-#
+# data_sources/__init__.py
+from .data_hub import DataSource, DataHub
+from .binance_ws import BinanceWebSocket
+from .finnhub_ws import FinnhubWebSocket
+from .finnhub_rest import FinnhubREST
+# Keep binance_rest for possible future use
+from .binance_rest import BinanceREST
+
+__all__ = [
+    'DataSource', 'DataHub',
+    'BinanceWebSocket', 'FinnhubWebSocket',
+    'BinanceREST', 'FinnhubREST'
+]
