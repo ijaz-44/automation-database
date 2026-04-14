dba85816a753de91714419942c897253a67380aa
ijaz-44 <123962078+ijaz-44@users.noreply.github.com>
2026-04-15 04:50:07 +0500

    🆕 Add my_automation_backup/Groups/group_x/X10_correlation_ws.py via Automation

---
+++ my_automation_backup/Groups/group_x/X10_correlation_ws.py
@@ -0,0 +1,11 @@
+# Groups/group_x/X10_correlation_ws.py
+class CorrelationWebSocket:
+    def __init__(self, base_dir):
+        self.base_dir = base_dir
+        print("[X10_correlation_ws] Placeholder – not used")
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
